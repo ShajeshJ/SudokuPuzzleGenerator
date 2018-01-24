edf7d25d28deb10d015773bae69d9fc8aef85fbf
SHAJESH JEGATHEESWARAN <s9jegath@uwaterloo.ca>
2018-01-23 20:04:53 -0500

    Add project files.

---
+++ Sudoku/GameRunner.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public class GameRunner
+    {
+        private Random _rng;
+
+        private int[] _solvedBoard;
+        private int[] _puzzleBoard;
+
+        private static Dictionary<int, IEnumerable<int>> _setLookup;
+
+        static GameRunner()
+        {
+            InitSetLookup();
+        }
+
+        public int[,] Board
+        {
+            get
+            {
+                var board = new int[9, 9];
+
+                for (int i = 0; i <= board.GetUpperBound(0); i++)
+                {
+                    for (int j = 0; j <= board.GetUpperBound(1); j++)
+                    {
+                        board[i, j] = _puzzleBoard[i * 9 + j];
+                    }
+                }
+
+                return board;
+            }
+        }
+
+        public GameRunner()
+        {
+            _rng = new Random();
+            CreateBoard();
+        }
+
+        public void CreateBoard()
+        {
+            _solvedBoard = new int[81];
+
+            for (int i = 0; i < _solvedBoard.Length; i++)
+            {
+                _solvedBoard[i] = -1;
+            }
+
+            _solvedBoard = FillBoard(0, _solvedBoard);
+            _puzzleBoard = CreatePuzzle(_solvedBoard);
+        }
+
+        private static void InitSetLookup()
+        {
+            List<List<int>> allSets = new List<List<int>>
+            {
+                new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8 },            //Row 1
+                new List<int> { 9, 10, 11, 12, 13, 14, 15, 16, 17 },    //Row 2
+                new List<int> { 18, 19, 20, 21, 22, 23, 24, 25, 26 },   //Row 3
+                new List<int> { 27, 28, 29, 30, 31, 32, 33, 34, 35 },   //Row 4
+                new List<int> { 36, 37, 38, 39, 40, 41, 42, 43, 44 },   //Row 5
+                new List<int> { 45, 46, 47, 48, 49, 50, 51, 52, 53 },   //Row 6
+                new List<int> { 54, 55, 56, 57, 58, 59, 60, 61, 62 },   //Row 7
+                new List<int> { 63, 64, 65, 66, 67, 68, 69, 70, 71 },   //Row 8
+                new List<int> { 72, 73, 74, 75, 76, 77, 78, 79, 80 },   //Row 9
+
+                new List<int> { 0, 9, 18, 27, 36, 45, 54, 63, 72 },     //Col 1
+                new List<int> { 1, 10, 19, 28, 37, 46, 55, 64, 73 },    //Col 2
+                new List<int> { 2, 11, 20, 29, 38, 47, 56, 65, 74 },    //Col 3
+                new List<int> { 3, 12, 21, 30, 39, 48, 57, 66, 75 },    //Col 4
+                new List<int> { 4, 13, 22, 31, 40, 49, 58, 67, 76 },    //Col 5
+                new List<int> { 5, 14, 23, 32, 41, 50, 59, 68, 77 },    //Col 6
+                new List<int> { 6, 15, 24, 33, 42, 51, 60, 69, 78 },    //Col 7
+                new List<int> { 7, 16, 25, 34, 43, 52, 61, 70, 79 },    //Col 8
+                new List<int> { 8, 17, 26, 35, 44, 53, 62, 71, 80 },    //Col 9
+
+                new List<int> { 0, 1, 2, 9, 10, 11, 18, 19, 20 },       //Box 1
+                new List<int> { 3, 4, 5, 12, 13, 14, 21, 22, 23 },      //Box 2
+                new List<int> { 6, 7, 8, 15, 16, 17, 24, 25, 26 },      //Box 3
+                new List<int> { 27, 28, 29, 36, 37, 38, 45, 46, 47 },   //Box 4
+                new List<int> { 30, 31, 32, 39, 40, 41, 48, 49, 50 },   //Box 5
+                new List<int> { 33, 34, 35, 42, 43, 44, 51, 52, 53 },   //Box 6
+                new List<int> { 54, 55, 56, 63, 64, 65, 72, 73, 74 },   //Box 7
+                new List<int> { 57, 58, 59, 66, 67, 68, 75, 76, 77 },   //Box 8
+                new List<int> { 60, 61, 62, 69, 70, 71, 78, 79, 80 }    //Box 9
+            };
+
+            _setLookup = new Dictionary<int, IEnumerable<int>>();
+
+            allSets.ForEach(x => x.ForEach(k =>
+            {
+                if (_setLookup.ContainsKey(k))
+                    _setLookup[k] = _setLookup[k].Union(x.Where(v => v != k));
+                else
+                    _setLookup[k] = new List<int>(x.Where(v => v != k));
+            }));
+        }
+
+        private int[] FillBoard(int cell, int[] board)
+        {
+            var validValues = new HashSet<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for (int i = 0; i < cell; i++)
+            {
+                if (_setLookup[cell].Contains(i))
+                {
+                    validValues.Remove(board[i]);
+                }
+            }
+
+            while (true)
+            {
+                if (validValues.Count == 0)
+                {
+                    return null;
+                }
+
+                board[cell] = validValues.ElementAt(_rng.Next(0, validValues.Count));
+                validValues.Remove(board[cell]);
+
+                if (cell == 80)
+                {
+                    return board;
+                }
+
+                var newBoard = FillBoard(cell + 1, board);
+
+                if (newBoard != null)
+                {
+                    return newBoard;
+                }
+            }
+        }
+
+        private int[] CreatePuzzle(int[] board)
+        {
+            var newBoard = new int[board.Length];
+
+            for (int i = 0; i < newBoard.Length; i++)
+            {
+                newBoard[i] = -1;
+            }
+
+            var cellsToTry = new HashSet<int>(Enumerable.Range(0, newBoard.Length));
+
+            while (cellsToTry.Count > 0)
+            {
+                var tryCell = cellsToTry.ElementAt(_rng.Next(0, cellsToTry.Count()));
+
+                var insert = false;
+
+                foreach (var matchCell in _setLookup[tryCell])
+                {
+                    if (newBoard[matchCell] == -1)
+                    {
+                        var validValues = new HashSet<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+                        foreach (var checkCell in _setLookup[matchCell])
+                        {
+                            validValues.Remove(newBoard[checkCell]);
+                        }
+
+                        if (validValues.Contains(board[tryCell]))
+                        {
+                            insert = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (insert)
+                {
+                    newBoard[tryCell] = board[tryCell];
+                }
+
+                cellsToTry.Remove(tryCell);
+            }
+
+            return newBoard;
+        }
+    }
+}
---
+++ Sudoku/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Drawing;
+using Microsoft.Win32;
+
+namespace Sudoku
+{
+    class Program
+    {
+        private static int elapsedTime;
+        static void Main(string[] args)
+        {
+            var runner = new GameRunner();
+            var board = runner.Board;
+
+            var boardImage = DrawBoard(board, ValidBoard(board));
+
+            boardImage.Save(@"sudoku.png");
+
+            var divider = "-------------";
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i%3 == 0)
+                {
+                    Console.WriteLine(divider);
+                }
+
+                for (int j = 0; j < 9; j++)
+                {
+                    if (j%3 == 0)
+                    {
+                        Console.Write("|");
+                    }
+                    Console.Write(board[i, j] == -1 ? "_" : ""+board[i, j]);
+                }
+                Console.WriteLine("|");
+            }
+            Console.WriteLine(divider);
+            //Console.ReadKey();
+        }
+
+        private static bool[,] ValidBoard(int[,] board)
+        {
+            var goodCells = new bool[9, 9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    goodCells[i, j] = true;
+                }
+            }
+
+            var checkHorz = new HashSet<int>();
+            var checkVert = new HashSet<int>();
+
+            var badRow = false;
+            var badCol = false;
+
+            for (int i = 0; i < 9; i++)
+            {
+                badRow = false;
+                badCol = false;
+                checkHorz.Clear();
+                checkVert.Clear();
+
+                for (int j = 0; j < 9; j++)
+                {
+                    if(board[i, j] != -1 && !checkHorz.Add(board[i, j]))
+                    {
+                        badRow = true;
+                    }
+
+                    if(board[j, i] != -1 && !checkVert.Add(board[j, i]))
+                    {
+                        badCol = true;
+                    }
+                }
+
+                if (badRow)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        goodCells[i, j] = false;
+                    }
+                }
+
+                if (badCol)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        goodCells[j, i] = false;
+                    }
+                }
+            }
+
+            var checkBox = new HashSet<int>();
+
+            for (int i = 0; i < 9; i+=3)
+            {
+                for (int j = 0; j < 9; j+=3)
+                {
+                    checkBox.Clear();
+
+                    var valid = (board[i, j] == -1 || checkBox.Add(board[i, j]))
+                                && (board[i + 1, j] == -1 || checkBox.Add(board[i + 1, j]))
+                                && (board[i + 2, j] == -1 || checkBox.Add(board[i + 2, j]))
+                                && (board[i, j + 1] == -1 || checkBox.Add(board[i, j + 1]))
+                                && (board[i + 1, j + 1] == -1 || checkBox.Add(board[i + 1, j + 1]))
+                                && (board[i + 2, j + 1] == -1 || checkBox.Add(board[i + 2, j + 1]))
+                                && (board[i, j + 2] == -1 || checkBox.Add(board[i, j + 2]))
+                                && (board[i + 1, j + 2] == -1 || checkBox.Add(board[i + 1, j + 2]))
+                                && (board[i + 2, j + 2] == -1 || checkBox.Add(board[i + 2, j + 2]));
+
+                    if (!valid)
+                    {
+                        goodCells[i, j] = false;
+                        goodCells[i + 1, j] = false;
+                        goodCells[i + 2, j] = false;
+                        goodCells[i, j + 1] = false;
+                        goodCells[i + 1, j + 1] = false;
+                        goodCells[i + 2, j + 1] = false;
+                        goodCells[i, j + 2] = false;
+                        goodCells[i + 1, j + 2] = false;
+                        goodCells[i + 2, j + 2] = false;
+                    }
+                }
+            }
+
+            return goodCells;
+        }
+
+        private static Image DrawBoard(int [,] board, bool[,] valid)
+        {
+            var tmpImg = new Bitmap(1, 1);
+            var tmpDrawing = Graphics.FromImage(tmpImg);
+
+            var font = new Font(FontFamily.GenericMonospace, 24, FontStyle.Bold, GraphicsUnit.Pixel);
+            var textBrush = new SolidBrush(Color.Black);
+
+            var textSize = tmpDrawing.MeasureString("1", font);
+
+            tmpImg.Dispose();
+            tmpDrawing.Dispose();
+
+            var goodImgs = new Bitmap[10];
+            var badImgs = new Bitmap[10];
+            var goodDrawings = new Graphics[10];
+            var badDrawings = new Graphics[10];
+
+            goodImgs[0] = new Bitmap((int)textSize.Width, (int)textSize.Height);
+            goodDrawings[0] = Graphics.FromImage(goodImgs[0]);
+            goodDrawings[0].Clear(Color.White);
+            goodDrawings[0].Save();
+            goodDrawings[0].Dispose();
+
+            badImgs[0] = new Bitmap((int)textSize.Width, (int)textSize.Height);
+            badDrawings[0] = Graphics.FromImage(badImgs[0]);
+            badDrawings[0].Clear(Color.PaleVioletRed);
+            badDrawings[0].Save();
+            badDrawings[0].Dispose();
+
+            for (int i = 1; i < goodImgs.Length; i++)
+            {
+                goodImgs[i] = new Bitmap((int)textSize.Width, (int)textSize.Height);
+                goodDrawings[i] = Graphics.FromImage(goodImgs[i]);
+                goodDrawings[i].Clear(Color.White);
+                goodDrawings[i].DrawString($"{i + 1}", font, textBrush, 0, 0);
+                goodDrawings[i].Save();
+                goodDrawings[i].Dispose();
+
+                badImgs[i] = new Bitmap((int)textSize.Width, (int)textSize.Height);
+                badDrawings[i] = Graphics.FromImage(badImgs[i]);
+                badDrawings[i].Clear(Color.PaleVioletRed);
+                badDrawings[i].DrawString($"{i + 1}", font, textBrush, 0, 0);
+                badDrawings[i].Save();
+                badDrawings[i].Dispose();
+            }
+
+            var tileWidth = goodImgs[0].Width;
+            var tileHeight = goodImgs[0].Height;
+
+            var boardImg = new Bitmap(tileWidth*9, tileHeight*9);
+            var boardDrawing = Graphics.FromImage(boardImg);
+
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    if (board[i, j] != -1)
+                    {
+                        boardDrawing.DrawImage(valid[i, j] ? goodImgs[board[i, j]] : badImgs[board[i, j]], j * tileWidth, i * tileHeight);
+                    }
+                    else
+                    {
+                        boardDrawing.DrawImage(valid[i, j] ? goodImgs[0] : badImgs[0], j * tileWidth, i * tileHeight);
+                    }
+                }
+            }
+
+            var smallPen = new Pen(textBrush, 1);
+            var bigPen = new Pen(textBrush, 3);
+
+            for(int i = 1; i < 9; i++)
+            {
+                boardDrawing.DrawLine(smallPen, tileWidth * i, 0, tileWidth * i, tileHeight * 9);
+                boardDrawing.DrawLine(smallPen, 0, tileHeight * i, tileWidth * 9, tileHeight * i);
+            }
+
+            for (int i = 0; i < 9; i+=3)
+            {
+                boardDrawing.DrawLine(bigPen, tileWidth * i, 0, tileWidth * i, tileHeight * 9);
+                boardDrawing.DrawLine(bigPen, 0, tileHeight * i, tileWidth * 9, tileHeight * i);
+            }
+            boardDrawing.DrawLine(bigPen, tileWidth * 9 - 1, 0, tileWidth * 9 - 1, tileHeight * 9);
+            boardDrawing.DrawLine(bigPen, 0, tileHeight * 9 - 1, tileWidth * 9, tileHeight * 9 - 1);
+
+            boardDrawing.Save();
+
+            return boardImg;
+        }
+    }
+}
